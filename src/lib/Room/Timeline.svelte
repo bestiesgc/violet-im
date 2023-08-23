6fe8cd70d02587e93fa3b1cd0adcee5ddb9e397a
hazycora <hazysu@riseup.net>
2023-08-22 21:33:41 -0500

    add autoscrolling

--- src/lib/Room/Timeline.svelte
+++ src/lib/Room/Timeline.svelte
@@ -6,7 +6,8 @@
 	export let room
 
 	let timelineElement
-	let timeline = room.getLiveTimeline().events
+	let activeTimeline = room.getLiveTimeline()
+	let timeline = activeTimeline.events
 	let wrappedTimeline = []
 
 	function onTimeline() {
@@ -15,12 +16,39 @@
 		})
 	}
 
+	async function loadPrevious() {
+		function getFirstLinkedTimeline() {
+			let tl = activeTimeline
+			while (tl?.getNeighbouringTimeline('b')) {
+				tl = tl.getNeighbouringTimeline('b')
+			}
+			return tl
+		}
+		await client.matrixClient.paginateEventTimeline(
+			getFirstLinkedTimeline(timeline),
+			{
+				backwards: true,
+				limit: 30
+			}
+		)
+		onTimeline()
+	}
+
+	function onScroll() {
+		if (timelineElement.scrollTop <= 100) loadPrevious()
+	}
+
 	onTimeline()
 
+	let oldestMessageId = timeline[0].getId()
+	let scrollPosition
+	let scrollHeight
 	let shouldScroll = true
 
 	beforeUpdate(() => {
 		if (timelineElement) {
+			scrollPosition = timelineElement.scrollTop
+			scrollHeight = timelineElement.scrollHeight
 			shouldScroll =
 				timelineElement.scrollTop + timelineElement.offsetHeight ==
 				timelineElement.scrollHeight
@@ -30,6 +58,11 @@
 	afterUpdate(() => {
 		if (shouldScroll) {
 			timelineElement.scrollTop = timelineElement.scrollHeight
+		} else {
+			if (timeline[0].getId() != oldestMessageId) {
+				timelineElement.scrollTop =
+					scrollPosition + (timelineElement.scrollHeight - scrollHeight)
+			}
 		}
 	})
 
@@ -41,7 +74,7 @@
 	})
 </script>
 
-<ol class="timeline scroller" bind:this={timelineElement}>
+<ol class="timeline scroller" bind:this={timelineElement} on:scroll={onScroll}>
 	<div style:margin-top="auto"></div>
 	{#each wrappedTimeline as event, i (event.getId())}
 		<li>
