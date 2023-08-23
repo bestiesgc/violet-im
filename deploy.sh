64b34d53a334ec4796e9bc166bb0adbc57514642
hazycora <hazysu@riseup.net>
2023-08-22 23:14:39 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cp -rT build built
 cd built
+rm -rf .git
+git init
+git remote add origin git@git.gay:besties/violet-im
+git checkout -b page
 git add -A
 git commit -m 'deploy'
-git push -u origin page
+git push -u origin page --force
