622ee23231d54821f3752e6eb5c42b90d3d439b6
hazycora <hazysu@riseup.net>
2023-09-01 03:26:09 -0500

    use manualChunks, add dummy rust-crypto

---
+++ src/dummy/rust-crypto/index.js
@@ -0,0 +1,2 @@
+// matrix-js-sdk imports this despite not using it, so we need to override it with a dummy module
+// otherwise we waste like 8 megabytes (lol, dreadful)
--- vite.config.js
+++ vite.config.js
@@ -30,5 +30,68 @@ export default defineConfig({
 				process: false
 			}
 		})
-	]
+	],
+	build: {
+		rollupOptions: {
+			external: [],
+			output: {
+				inlineDynamicImports: false,
+				manualChunks(_id) {
+					if (!_id.includes('node_modules')) {
+						return
+					}
+					if (
+						_id.includes('svelte') ||
+						_id.includes('cookie') ||
+						_id.includes('devalue') ||
+						_id.includes('esm-env') ||
+						_id.includes('mousetrap')
+					) {
+						return 'svelte'
+					}
+					if (
+						_id.includes('vite-plugin-node-polyfills') ||
+						_id.includes('node-stdlib-browser') ||
+						_id.includes('buffer') ||
+						_id.includes('browserify') ||
+						_id.includes('uuid') ||
+						_id.includes('base64-js') ||
+						_id.includes('ieee754') ||
+						_id.includes('readable-stream') ||
+						_id.includes('string_decoder') ||
+						_id.includes('sha.js')
+					) {
+						return 'node-polyfills'
+					}
+					if (_id.includes('highlight.js')) {
+						return 'highlight'
+					}
+					if (
+						_id.includes('matrix-js-sdk') ||
+						(_id.includes('babel') && _id.includes('runtime'))
+					) {
+						return 'matrix-js-sdk'
+					}
+					if (
+						_id.includes('matrix-widget-api') ||
+						_id.includes('matrix-events-sdk') ||
+						_id.includes('matrix-encrypt-attachment')
+					) {
+						return 'matrix-etc'
+					}
+					if (
+						_id.includes('crypto') ||
+						_id.includes('hash.js') ||
+						_id.includes('elliptic') ||
+						_id.includes('asn1') ||
+						_id.includes('diffie-hellman') ||
+						_id.includes('encrypt')
+					) {
+						return 'crypto'
+					}
+					return 'misc'
+				}
+			}
+		}
+	}
 })
